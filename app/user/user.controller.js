bbffc2044b7a0c3b87903947787cd41c2425ee22
Jonathan Rodriguez Sanchez <jonathan.rodriguezs@softtek.com>
2020-09-11 13:01:43 -0500

    feat: modify user create controller to send email

--- app/user/user.controller.js
+++ app/user/user.controller.js
@@ -2,6 +2,9 @@ import Model from './user'
 import { Router } from 'express'
 import { red, green, blue } from './../../helpers/chalk.helper'
 import bcrypt from 'bcryptjs'
+import { generatePassword } from '../auth/auth.helper'
+import { generateMailer } from '../../mailer'
+import { ENV } from '../../keys'
 const router = new Router()
 
 export function userController(Collection) {
@@ -10,7 +13,7 @@ export function userController(Collection) {
   // ======
   router.post('/', create)
   router.get('/', readMany)
-  router.get('/:_id', readOne)
+  // router.get('/:_id', readOne)
   router.put('/:_id', update)
   router.delete('/:_id', remove)
 
@@ -21,10 +24,22 @@ export function userController(Collection) {
     try {
       blue('users > controller > create')
       const newEntry = req.body
+      const password = generatePassword(10)
       const salt = await bcrypt.genSalt(10)
       const hashed = await bcrypt.hash(password, salt)
       const result = await Collection.create({ ...newEntry, password: hashed })
-      return res.send(result.select('-password'))
+
+      const hostname = ENV === 'development' ? 'localhost:5001' : host
+      const url = `http://${hostname}/login`
+
+      const mailer = await generateMailer()
+      await mailer.send({
+        template: 'register',
+        message: { to: result.email },
+        locals: { url, password }
+      })
+
+      return res.send(result)
     } catch (error) {
       red(error)
       return res.status(500).send(error)
@@ -37,34 +52,29 @@ export function userController(Collection) {
   async function readMany(req, res) {
     try {
       blue('users > controller > readMany')
-      let query = res.locals.query || {}
-      const result = await Collection.find(query)
+      const query = req.query || {}
+      const result = await Collection.find(query).select('-password')
       green(result)
-      res.send(result)
-      return
+      return res.json(result)
     } catch (error) {
       red(error)
-      res.status(500).send(error)
-      return
+      return res.status(500).send(error)
     }
   }
 
   // ========
   // Read one
   // ========
-
   async function readOne(req, res) {
     try {
       blue('users > controller > readOne')
       const { _id } = req.params
-      const result = await Collection.findById(_id)
+      const result = await Collection.findById(_id).select('-password')
       green(result)
-      res.send(result)
-      return
+      return res.send(result)
     } catch (error) {
       red(error)
-      res.status(500).send(error)
-      return
+      return res.status(500).send(error)
     }
   }
 
@@ -74,28 +84,15 @@ export function userController(Collection) {
   async function update(req, res) {
     try {
       blue('users > controller > update')
-      //fields validations
-      if (false) {
-        res.status(400).send({ message: 'validation message' })
-        return
-      }
-
-      // model validations
-      if (false) {
-        res.status(400).send({ message: 'validation message' })
-        return
-      }
-
       const changedEntry = req.body
       const { _id } = req.params
-      const result = await Collection.update({ _id }, { $set: changedEntry })
+      delete changedEntry.password
+      const result = await Collection.update({ _id }, changedEntry, { new: true })
       green(result)
-      res.send(result)
-      return
+      return res.send(result)
     } catch (error) {
       red(error)
-      res.status(500).send(error)
-      return
+      return res.status(500).send(error)
     }
   }
 
@@ -105,27 +102,13 @@ export function userController(Collection) {
   async function remove(req, res) {
     try {
       blue('users > controller > remove')
-      //fields validations
-      if (false) {
-        res.status(400).send({ message: 'validation message' })
-        return
-      }
-
-      // model validations
-      if (false) {
-        res.status(400).send({ message: 'validation message' })
-        return
-      }
-
       const { _id } = req.params
       const result = await Collection.remove(_id)
       green(result)
-      res.send(result)
-      return
+      return res.send(result)
     } catch (error) {
       red(error)
-      res.status(500).send(error)
-      return
+      return res.status(500).send(error)
     }
   }
 
